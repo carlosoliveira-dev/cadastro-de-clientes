1824f3b3391f2252bc32d9ed45623c1ba27d2681
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-12-04 15:22:03 -0300

    configuração inicial do servidor e arquivos

---
+++ back/src/core/shared/error.ts
@@ -0,0 +1,7 @@
+const Error = {
+    USUARIO_JA_EXISTE: 'USUARIO_JA_EXISTE',
+    USUARIO_NAO_EXISTE: 'USUARIO_NAO_EXISTE',
+    SENHA_INCORRETA: 'SENHA_INCORRETA',
+} as const
+
+export default Error
---
+++ back/src/core/user/model/user.ts
@@ -0,0 +1,6 @@
+export default interface Usuario {
+    nome: string
+    email: string
+    id?: string
+    senha?: string
+}
