a16835cb459fd080d67b00b44dee10e2a1bc7941
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-12-06 11:43:52 -0300

    instanciação do caso de uso RegisterUser adicionada no server

--- back/src/external/api/app.ts
+++ back/src/external/api/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express + TypeScript!");
--- back/src/external/api/server.ts
+++ back/src/external/api/server.ts
@@ -1,6 +1,20 @@
+import RepositoryUserPg from '@/external/db/repository-user-pg'
+import RegisterUser from '@/core/user/service/register-user';
 import app from './app';
-import "dotenv/config";
+import dotenv from "dotenv"
+dotenv.config()
 
-app.listen(3000, () => {
+const port = process.env.API_PORT ?? 4000
+
+app.listen(port, () => {
   console.log("Server running on http://localhost:3000");
 });
+
+// Public Routes
+
+const repositoryUser = new RepositoryUserPg()
+const registerUser = new RegisterUser(repositoryUser)
+
+
+// Protected Routes
+
