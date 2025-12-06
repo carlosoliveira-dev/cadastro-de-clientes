75d66ae69d5945cf2e1199b0b008047069c3ff5d
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-12-06 11:55:30 -0300

    register-user-controller adicionado com uma configuração mínima

---
+++ back/src/external/api/register-user-controller.ts
@@ -0,0 +1,23 @@
+import User from "@/core/user/model/user"
+import RegisterUser from "@/core/user/service/register-user"
+import { Express } from "express"
+
+export default class RegistrarUsuarioController {
+    constructor(
+        server: Express,
+        usecase: RegisterUser
+    ) {
+        server.post('/api/usuarios/registrar', async (req, resp) => {
+            try {
+                await usecase.execute({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password,
+                } as User)
+                resp.status(201).send()
+            } catch(erro: any) {
+                resp.status(400).send(erro.message)
+            }
+        })
+    }
+}
