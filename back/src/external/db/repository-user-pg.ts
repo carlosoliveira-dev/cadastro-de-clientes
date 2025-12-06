eb89319f237db2dbb3f3ace217fe78de7ef06644
Carlos <carlos.oliveira.dev.junior@gmail.com>
2025-12-06 11:18:39 -0300

    interface adicionada

--- back/src/external/db/repository-user-pg.ts
+++ back/src/external/db/repository-user-pg.ts
@@ -1,7 +1,8 @@
 import User from "@/core/user/model/user"
+import RepositoryUser from "@/core/user/service/repository-user"
 import db from "./db"
 
-export default class RepositoryUserPg {
+export default class RepositoryUserPg implements RepositoryUser{
     async add(user: User) {
         await db.query(
             `insert into users
